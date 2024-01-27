f51d8371be6c3a83e9b945df2de619098edf6d90
Julesntizimira <ntizimijules5@gmail.com>
2024-01-27 10:48:46 +0200

    added a readme file

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,6 +7,6 @@ def index_range(page, page_size):
     '''return a tuple of size two containing
        a start index and an end index
     '''
-    a = (page - 1) * page_size
-    b = (a + page_size)
-    return (a, b)
+    start = (page - 1) * page_size
+    end = (start + page_size)
+    return (start, end)
