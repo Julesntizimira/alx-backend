15072cbdac47a73638414de281951a1eeecc3835
Julesntizimira <ntizimijules5@gmail.com>
2024-02-06 14:51:34 +0200

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -16,13 +16,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-
-
 def get_locale():
     '''get local language'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel = Babel(app, locale_selector=get_locale)
+
 
 @app.route('/', strict_slashes=False)
 def home():
