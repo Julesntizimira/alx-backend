db865e4b3eb29003e42a1f681d0254d471679821
Julesntizimira <ntizimijules5@gmail.com>
2024-02-06 14:00:49 +0200

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,7 +19,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
