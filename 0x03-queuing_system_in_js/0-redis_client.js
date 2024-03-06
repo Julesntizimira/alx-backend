7170558c8adcff206ac83bc33cf808a4c39298c6
Julesntizimira <ntizimijules5@gmail.com>
2024-03-06 21:08:40 +0200

    js queuing

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+const { createClient } = require('redis');
+
+const client = createClient();
+
+// Listen for errors
+client.on('error', err => console.log(`Redis client error: ${err.message}`));
+console.log('Redis client connected to the server');
